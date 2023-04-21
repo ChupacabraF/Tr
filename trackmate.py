37e23af3001fb8fd356147329d54e6a31d604fdc
ChupacabraF <flo.vonraesfeld@gmail.com>
2023-04-21 09:03:48 +0200

    Erster Commit - Login Oberfläche

---
+++ trackmate.py
@@ -0,0 +1,35 @@
+import customtkinter as ctk
+
+def login():      
+
+    # login call
+    print("logging in")
+
+def register():      
+
+    # register call
+    print("registering")
+
+
+ctk.set_default_color_theme("green")
+
+root = ctk.CTk()
+root.geometry("960x540")
+root.title("YouTube to .mp3 Converter")
+
+title_label = ctk.CTkLabel(root, text="TrackMate", font=ctk.CTkFont(size=30, weight="bold"))
+title_label.pack(padx=10, pady=(40, 20))
+
+username_entry = ctk.CTkEntry(root, placeholder_text="Username", width=400, height=50)
+username_entry.pack(pady=8, padx=20)
+
+password_entry = ctk.CTkEntry(root, placeholder_text="Password", width=400, height=50)
+password_entry.pack(pady=8, padx=20,)
+
+login_button = ctk.CTkButton(root, text="Login", width=300, height=35, command=login)
+login_button.pack(pady=16, padx=20)
+
+login_button = ctk.CTkButton(root, text="Register", width=200, height=35, command=register)
+login_button.pack(pady=100, padx=20)
+
+root.mainloop()
